22f32a6f228baac2a2b1be88025d2a2dc684a9cb
RickyAST <rickyast21@gmail.com>
2025-07-16 00:51:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import openai
 
 app = Flask(__name__)
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = os.environ.get("OPENROUTER_API_KEY")
 
 @app.route("/chat", methods=["POST"])
 def chat():
